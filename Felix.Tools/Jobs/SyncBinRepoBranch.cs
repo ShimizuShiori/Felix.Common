48a2805c8940f293d34297e3a4c113deae15862e
Felix Fei <Felix.Fei@wisetechglobal.com>
2022-12-06 08:46:26 +0800

    Remove unused files\

--- Felix.Tools/Jobs/SyncBinRepoBranch.cs
+++ Felix.Tools/Jobs/SyncBinRepoBranch.cs
@@ -1,29 +1,30 @@
-﻿using Felix.Common;
+﻿//using Felix.Common;
 
-namespace Felix.Tools.Jobs
-{
-	class SyncBinRepoBranch : IJob
-	{
-		const string WatchedRepo = @"C:\git\wtg\CargoWise\Dev";
-		const string BinRepo = @"C:\BinRepo";
-		public async Task OnStart(CancellationToken cancellationToken)
-		{
-			var runnerForWatched = new GitRunner(WatchedRepo);
-			var runnerForBinRepo = new GitRunner(BinRepo);
-			while (true)
-			{
-				await Task.Delay(TimeSpan.FromSeconds(5));
-				if (cancellationToken.IsCancellationRequested)
-					break;
+//namespace Felix.Tools.Jobs
+//{
+//	class SyncBinRepoBranch : IJob
+//	{
+//		const string WatchedRepo = @"C:\git\wtg\CargoWise\Dev";
+//		const string BinRepo = @"C:\BinRepo";
+//		public async Task OnStart(CancellationToken cancellationToken)
+//		{
+//			var runnerForWatched = new GitRunner(WatchedRepo);
+//			var runnerForBinRepo = new GitRunner(BinRepo);
+//			while (true)
+//			{
+//				await Task.Delay(TimeSpan.FromSeconds(5));
+//				if (cancellationToken.IsCancellationRequested)
+//					break;
 
-				var watchedBranch = runnerForWatched.GetCurrentBranch();
-				var binRepoBranch = runnerForBinRepo.GetCurrentBranch();
-				if (watchedBranch == binRepoBranch)
-					continue;
+//				var watchedBranch = runnerForWatched.GetCurrentBranch();
+//				var binRepoBranch = runnerForBinRepo.GetCurrentBranch();
+//				if (watchedBranch == binRepoBranch)
+//					continue;
 
-				runnerForBinRepo.CreateBranch(watchedBranch);
-				runnerForBinRepo.SwitchBranch(watchedBranch);
-			}
-		}
-	}
-}
+//				runnerForBinRepo.CreateBranch(watchedBranch);
+//				runnerForBinRepo.SwitchBranch(watchedBranch);
+//			}
+//		}
+//	}
+//}
+w
