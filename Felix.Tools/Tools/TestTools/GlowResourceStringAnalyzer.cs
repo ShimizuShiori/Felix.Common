d51238ad4e543b37029c272d0d228839c6768d73
Felix Fei <Felix.Fei@wisetechglobal.com>
2023-01-12 12:30:45 +0800

    no message

---
+++ Felix.Tools/Tools/TestTools/GlowResourceStringAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using Felix.Tools.Attributes;
+using System.Diagnostics;
+
+namespace Felix.Tools.Tools.TestTools
+{
+	[Tool("ResourceStringAnalyzer", "Tests")]
+	class GlowResourceStringAnalyzer : ITool
+	{
+		public Task StartAsync()
+		{
+			using (var p = new Process())
+			{
+				p.StartInfo.FileName = @"C:\git\wtg\Glow\Glow\Language\Bin\Analyzer\net472\ResourceStringAnalyzer.exe";
+				p.StartInfo.Arguments = @"C:\git\wtg\Glow\Glow\DotNet";
+				p.Start();
+
+				p.WaitForExit();
+			}
+			using (var p = new Process())
+			{
+				p.StartInfo.FileName = @"C:\git\wtg\Glow\Glow\DotNet\Bin\UncaptionedStrings.xml";
+				p.StartInfo.UseShellExecute = true;
+				p.Start();
+			}
+			return Task.CompletedTask;
+		}
+	}
+}
