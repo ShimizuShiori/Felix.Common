c49658325466dd56a7fd5af04048f36eb3726ed3
Felix Fei <Felix.Fei@wisetechglobal.com>
2023-01-17 14:19:43 +0800

    no message

---
+++ Felix.Tools/Tools/TextToBoxTool.cs
@@ -0,0 +1,31 @@
+﻿using Felix.Tools.Attributes;
+using System.Text;
+
+namespace Felix.Tools.Tools
+{
+	[TextTool("Box", "To")]
+	class TextToBoxTool : ITool
+	{
+		public Task StartAsync()
+		{
+			int length = AppContext.SelectedText.Length;
+			var sb = new StringBuilder((length + 4) * 3);
+			sb.Append("+");
+			for (int i = 0; i < length + 2; i++)
+				sb.Append("-");
+			sb.AppendLine("+");
+
+			sb.Append("| ");
+			sb.Append(AppContext.SelectedText);
+			sb.AppendLine(" |");
+
+			sb.Append("+");
+			for (int i = 0; i < length + 2; i++)
+				sb.Append("-");
+			sb.AppendLine("+");
+
+			OutputBox.Show(sb.ToString());
+			return Task.CompletedTask;
+		}
+	}
+}
