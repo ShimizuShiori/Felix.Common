3e5086da32a724d1f2018c5be7178b2b51ee2cb3
Felix Fei <Felix.Fei@wisetechglobal.com>
2023-01-29 08:42:04 +0800

    DataSetVIewer

---
+++ Felix.Tools/Forms/TableInfoForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Felix.Tools.Forms
+{
+	public partial class TableInfoForm : Form
+	{
+		readonly DataSet? set;
+		static readonly string[] Tabs = new string[]
+		{
+			"Table",
+			"Field",
+			"Identity",
+			"RowGuidColumn",
+			"FileGroup",
+			"Index",
+			"Constraint",
+			"Foreign Key"
+		};
+		public TableInfoForm() : this(null)
+		{
+		}
+		public TableInfoForm(DataSet? set)
+		{
+			this.set = set;
+			InitializeComponent();
+			tabControl1.TabPages.Clear();
+			if (set == null)
+				return;
+
+			for (int i = 0; i < Tabs.Length && i < set.Tables.Count; i++)
+			{
+				var tabName = Tabs[i];
+				var page = new TabPage(tabName);
+				var dataGridView = new DataGridView();
+				dataGridView.Dock = DockStyle.Fill;
+				dataGridView.DataSource = set.Tables[i];
+
+				tabControl1.TabPages.Add(page);
+				page.Controls.Add(dataGridView);
+			}
+		}
+
+
+
+		public static void Show(DataSet set)
+		{
+			TableInfoForm tableInfoForm = new TableInfoForm(set);
+			tableInfoForm.ShowDialog();
+		}
+	}
+}
---
+++ Felix.Tools/TestForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Felix.Tools
+{
+	partial class TestForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.tableInfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.tableInfoBindingSource)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(372, 286);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(188, 58);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// TestForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1431, 853);
+			this.Controls.Add(this.button1);
+			this.Name = "TestForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "TestForm";
+			((System.ComponentModel.ISupportInitialize)(this.tableInfoBindingSource)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private BindingSource tableInfoBindingSource;
+		private Button button1;
+	}
+}
