e9bdba7a5c66ac49694016a065f44bb3ec3ec493
Felix Fei <Felix.Fei@wisetechglobal.com>
2023-01-30 15:52:05 +0800

    Change the order of copy and cancel

--- Felix.Tools/OutputBox.Designer.cs
+++ Felix.Tools/OutputBox.Designer.cs
@@ -71,7 +71,7 @@
 			this.button1.Location = new System.Drawing.Point(603, 3);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(188, 58);
-			this.button1.TabIndex = 0;
+			this.button1.TabIndex = 1;
 			this.button1.Text = "Close";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -81,7 +81,7 @@
 			this.button2.Location = new System.Drawing.Point(409, 3);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(188, 58);
-			this.button2.TabIndex = 1;
+			this.button2.TabIndex = 0;
 			this.button2.Text = "Copy";
 			this.button2.UseVisualStyleBackColor = true;
 			this.button2.Click += new System.EventHandler(this.button2_Click);
