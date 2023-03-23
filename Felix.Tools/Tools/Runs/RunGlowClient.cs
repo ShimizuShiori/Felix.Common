f7c865aea8faf32f5bfedc15649ce83c674375f2
Felix Fei <Felix.Fei@wisetechglobal.com>
2023-03-23 13:17:34 +0800

    Glows

---
+++ Felix.Tools/Tools/Runs/RunGlowClient.cs
@@ -0,0 +1,15 @@
+﻿using Felix.Common;
+using Felix.Tools.Attributes;
+
+namespace Felix.Tools.Tools.Runs
+{
+	[Tool("HtmlClient", "Glow")]
+	class RunGlowClient : ITool
+	{
+		public void Start()
+		{
+			ProcessHelper.Start("dotnet", @"run --project C:\git\wtg\Glow\Glow\DotNet\HTML\Client\Client\Client.csproj");
+			UrlHelper.Open("https://localhost:59415/CST");
+		}
+	}
+}
