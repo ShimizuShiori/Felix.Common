3044c3356048727f999e8b81f1a15787aa9f39ea
Felix Fei <Felix.Fei@wisetechglobal.com>
2023-05-04 16:01:55 +0800

    no message

---
+++ Felix.Common/FastCopier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Felix.Common
+{
+	public static class FastCopier
+	{
+		public enum Comparer
+		{
+			Time = 1,
+			Length = 2,
+			MD5 = 4,
+			TimeAndLength = Time + Length
+		}
+
+		public enum Result
+		{
+			Created,
+			Copied,
+			Skipped
+		}
+
+		public static void Copy(string source, string dist, Comparer comparer = Comparer.TimeAndLength, Action<string, Result> reporter)
+		{
+
+		}
+	}
+}
